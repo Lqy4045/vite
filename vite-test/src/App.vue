8f211e662521d4aca667f321f971659f92241fb6
liqingyu <liqingyu@ata.net.cn>
2024-05-24 16:47:40 +0800

    add:vite配置vue

---
+++ vite-test/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="warpper">hello vue</div>
+  <p>{{ num }}</p>
+  <button @click="add">+</button>
+</template>
+<script setup>
+import { ref } from "vue";
+let num = ref(1);
+function add() {
+  num.value += 1;
+}
+</script>
+<style lang="less">
+.warpper {
+  color: var(--blueColor);
+}
+</style>
--- vite-test/src/main.js
+++ vite-test/src/main.js
@@ -1,3 +1,5 @@
+import { createApp } from "vue";
+import App from "./App.vue";
 import _ from "lodash-es";
 console.log("当前环境变量", import.meta.env);
 import "@/styles/variable.css";
@@ -6,3 +8,5 @@ import "@/components/componentB.js";
 import "@/assets/images/imageLoader.js";
 import "@/assets/json/jsonLoader.js";
 import "@/assets/svg/svgLoader.js";
+
+createApp(App).mount("#app");
